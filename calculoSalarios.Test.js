a0d21f5a65c44d3cfe014d9f09915a6420a12e97
Zyon <zyonamadeus01@gmail.com>
2021-10-10 20:32:55 -0300

    refatorado teste 2

--- calculoSalarios.Test.js
+++ calculoSalarios.Test.js
@@ -12,8 +12,7 @@ describe('calculoSalarios', () => {
     test('Desconto DBA', () => {
         cargo = 2;
         salario = 1500.00;
-        //salarioFinal = calculoSalarios.calcularSalario(cargo, salario);
-        salarioFinal = 1275.00;
+        salarioFinal = calculoSalarios.calcularSalario(cargo, salario);
         expect(salarioFinal).toBeCloseTo(1275.00);
     })
 })
--- calculoSalarios.js
+++ calculoSalarios.js
@@ -3,6 +3,11 @@ function calcularSalario(cargo, salario){
         case 1: //desenvolvedor
             if(salario >= 3000.00) salario -= (salario * 0.20);
             else salario -= (salario * 0.10);
+            break;
+        case 2:
+            if(salario >= 2000.00) salario -= (salario * 0.25);
+            else salario -= (salario * 0.15);
+            break;
     }
 }
 
