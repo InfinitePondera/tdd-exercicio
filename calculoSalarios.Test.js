59034d7d1664ac146759eaa10ea4176ba1c34b30
Zyon <zyonamadeus01@gmail.com>
2021-10-10 20:22:01 -0300

    refatorado corretamente para teste 1

--- calculoSalarios.Test.js
+++ calculoSalarios.Test.js
@@ -5,8 +5,7 @@ describe('calculoSalarios', () => {
     test('Desconto desenvolvedor', () => {
         cargo = 1;
         salario = 3000.00;
-        //salarioFinal = calculoSalarios.calcularSalario(cargo, salario);
-        salarioFinal = 2400.00;
+        salarioFinal = calculoSalarios.calcularSalario(cargo, salario);
         expect(salarioFinal).toBeCloseTo(2400.00);
     })
 })
